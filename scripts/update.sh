0846a3556b61ef9f1b0efa2b08f625cc873a02cd
Wes Cruver <chieffancypants@gmail.com>
2023-03-24 14:31:29 -0700

    make update script work from any directory

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env bash
 
-#
-# Run this file from the project's root directory (not scripts)
-#
-
-# TODO: Make sure this script can be run from the scripts directory
-
 BASEURL="https://github.com/chieffancypants/ddns-client"
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LATEST_TAG=`curl -si $BASEURL/releases/latest | sed -En 's/^location:.*tag\/(.*)\r/\1/p'`
 PACKAGE_URL="${BASEURL}/releases/download/${LATEST_TAG}/ddns-client.zip"
 
-DIR_CANONICAL=$(cd "$path"; pwd)
-cd $DIR_CANONICAL
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR_PROJ_ROOT=$(cd "$DIR/.."; pwd)
+cd $DIR_PROJ_ROOT
 
 # Try to be safe about rm -rf by asking for confirmation to the full path
-echo "Updating to $LATEST_TAG"
-echo "This will remove all files in $DIR_CANONICAL (with the exception of .env)"
+echo "Updating to $LATEST_TAG:"
+echo -e "  $PACKAGE_URL\n"
+echo "This will remove all files in $DIR_PROJ_ROOT (with the exception of .env)"
 read -p "Continue [y/N] " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    rm -rv $DIR_CANONICAL/*
+    rm -rv $DIR_PROJ_ROOT/*
 else
-    echo -e "\n\nAborted"
+    echo -e "\n\nAborted."
     exit 1
 fi
 
 curl -sL $PACKAGE_URL --output ddns-client.zip
 unzip ddns-client.zip
-
+rm ddns-client.zip
